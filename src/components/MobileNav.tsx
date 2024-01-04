9a88f8f85d448a38148f10e666702edf29e94c80
Eduard Tanase <eduard.tanase1@gmail.com>
2024-01-04 14:23:22 -0700

    Mobile nav tweaks

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from './ui/sheet';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import { Menu } from 'lucide-react';
-import NavItem from './NavItem';
 import { buttonVariants } from './ui/button';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+
+const navItems = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'About Us',
+    href: '/about',
+  },
+  {
+    name: 'FAQ',
+    href: '/faq',
+  },
+  {
+    name: 'Resources',
+    href: '/resources',
+  },
+  {
+    name: 'Contact Us',
+    href: '/contact',
+  },
+];
+
+const MobileNavItem = ({ name, href }: { name: string; href: string }) => {
+  return (
+    <SheetTrigger asChild>
+      <Link
+        href={href}
+        className={cn(
+          buttonVariants({
+            variant: 'link',
+            className: 'w-full text-4xl',
+          }),
+          'text-foreground'
+        )}
+      >
+        {name}
+      </Link>
+    </SheetTrigger>
+  );
+};
 
 const MobileNav = () => {
   return (
@@ -24,83 +58,16 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className='flex w-full flex-col sm:max-w-lg'>
         <div className='flex h-full flex-col items-center justify-center space-y-10'>
-          <SheetTrigger asChild>
-            <Link
-              href='/'
-              className={cn(
-                buttonVariants({
-                  variant: 'link',
-                  className: 'w-full text-3xl',
-                }),
-                'text-foreground'
-              )}
-            >
-              Home
-            </Link>
-          </SheetTrigger>
-          <SheetTrigger asChild>
-            <Link
-              href='/about'
-              className={cn(
-                buttonVariants({
-                  variant: 'link',
-                  className: 'w-full text-3xl',
-                }),
-                'text-foreground'
-              )}
-            >
-              About Us
-            </Link>
-          </SheetTrigger>
-          <SheetTrigger asChild>
-            <Link
-              href='/faq'
-              className={cn(
-                buttonVariants({
-                  variant: 'link',
-                  className: 'w-full text-3xl',
-                }),
-                'text-foreground'
-              )}
-            >
-              FAQ
-            </Link>
-          </SheetTrigger>
-          <SheetTrigger asChild>
-            <Link
-              href='/resources'
-              className={cn(
-                buttonVariants({
-                  variant: 'link',
-                  className: 'w-full text-3xl',
-                }),
-                'text-foreground'
-              )}
-            >
-              Resources
-            </Link>
-          </SheetTrigger>
-          <SheetTrigger asChild>
-            <Link
-              href='/contact'
-              className={cn(
-                buttonVariants({
-                  variant: 'link',
-                  className: 'w-full text-3xl',
-                }),
-                'text-foreground'
-              )}
-            >
-              Contact Us
-            </Link>
-          </SheetTrigger>
+          {navItems.map((item) => (
+            <MobileNavItem key={item.name} name={item.name} href={item.href} />
+          ))}
           <SheetTrigger asChild>
             <Link
               href='/donate'
               className={cn(
                 buttonVariants({
                   variant: 'link',
-                  className: 'w-full text-3xl',
+                  className: 'w-full text-4xl',
                 }),
                 'text-primary'
               )}
