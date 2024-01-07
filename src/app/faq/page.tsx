1fbf8aef9437b1606b52bbda9abc28fcbe4960e9
Eduard Tanase <eduard.tanase1@gmail.com>
2024-01-07 13:23:07 -0700

    FAQ

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,9 +1,31 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 
+const questions = [
+  {
+    question: 'Question?',
+    answer: 'answer',
+  },
+  {
+    question: 'Question?',
+    answer: 'answer',
+  },
+  {
+    question: 'Question?',
+    answer: 'answer',
+  },
+];
+
 export default function Page() {
   return (
-    <MaxWidthWrapper>
-      <p>faq</p>
+    <MaxWidthWrapper className='mt-5'>
+      {questions.map((question, i) => (
+        <div className='mb-5' key={i}>
+          <h3 className='text-xl text-muted-foreground border-b'>
+            {question.question}
+          </h3>
+          <p className='ml-5 text-md'>{question.answer}</p>
+        </div>
+      ))}
     </MaxWidthWrapper>
   );
 }
