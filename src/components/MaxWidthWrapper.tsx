515a5809da5565c28e89b0f371e0b214b397fb93
Eduard Tanase <eduard.tanase1@gmail.com>
2024-07-29 12:11:54 -0700

    chore: format

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -8,7 +8,11 @@ const MaxWidthWrapper = ({
   className?: string;
   children: ReactNode;
 }) => {
-  return <div className={cn('container mb-10 animate-fade-in-load', className)}>{children}</div>;
+  return (
+    <div className={cn('container mb-10 animate-fade-in-load', className)}>
+      {children}
+    </div>
+  );
 };
 
 export default MaxWidthWrapper;
