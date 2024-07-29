dc24deeebd4685d81a229ba28bd5280071ffd108
Eduard Tanase <eduard.tanase1@gmail.com>
2024-07-29 12:11:33 -0700

    Fade in animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,15 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'fade-in-load': {
+          from: { opacity: '0', transform: 'translateY(10px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in-load': 'fade-in-load 0.3s ease-in-out',
       },
     },
   },
